7350c186c51e2ce6de6ece2f5e8dcd4b73ebbff8
Battleship Guy <lawoso@gmail.com>
2023-09-20 13:49:37 +0200

    Initialize spring boot project

---
+++ src/main/java/nl/habiboellah/battleship/BattleshipApplication.java
@@ -0,0 +1,13 @@
+package nl.habiboellah.battleship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleshipApplication.class, args);
+	}
+
+}
